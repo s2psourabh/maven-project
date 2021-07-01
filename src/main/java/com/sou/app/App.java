a69259f0a12d79f7b0d67a94c355f3d7ffc26699
s2psourabh <s2psourabh@gmail.com>
2021-07-01 20:05:52 +0000

    first commit

---
+++ src/main/java/com/sou/app/App.java
@@ -0,0 +1,13 @@
+package com.sou.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
